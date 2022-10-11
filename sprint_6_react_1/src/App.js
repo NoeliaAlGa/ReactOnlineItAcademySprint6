289fa7a37b716786ea0828dd6e0779d71fae27ca
NoeliaAlvarez <noelia.ag1990@gmail.com>
2022-10-11 12:10:19 +0200

    Public y SRC

---
+++ sprint_6_react_1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Escena from './components/escena/Escena'
+
+function App() {
+  return (
+    <div className="App">
+      <header> 
+        <Escena/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
