5f28e992e8621ec76a681f9fb2a1c0a4afa3d5ec
NoeliaAlvarez <noelia.ag1990@gmail.com>
2022-10-16 14:33:44 +0200

    N2 E5 Benvinguda

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,51 @@
-import './App.css';
-import Escena from './components/escena/Escena';
-import {textos} from './components/escena/Textos'
-import {Buttons} from './components/styled';
-import {useState} from 'react';
+import "./App.css";
+import Escena from "./components/escena/Escena";
+import Cabecera from "./components/cabecera"
+import { textos } from "./components/escena/Textos";
+import { Buttons } from "./components/styled";
+import { useState } from "react";
 
 function App() {
- const [currentActive, setCurrentActive] = useState(0);
+  const [currentActive, setCurrentActive] = useState(0);
+  const [pantallaBenvinguda, setpantallaBenvinguda] = useState(true);
+
+  const iniciar = () => {
+    setpantallaBenvinguda(false);
+  };
 
   const back = () => {
-    if(currentActive > 0 && currentActive < textos.length) {
+    if (currentActive > 0 && currentActive < textos.length) {
       setCurrentActive(currentActive - 1);
     }
-  }
-  
+  };
+
   const next = () => {
-      if(currentActive >= 0 && currentActive < textos.length -1) {
-        setCurrentActive(currentActive + 1);
-      }
-  }
+    if (currentActive >= 0 && currentActive < textos.length - 1) {
+      setCurrentActive(currentActive + 1);
+    }
+  };
 
   return (
     <div className="App">
-        <div>
+      {pantallaBenvinguda
+        ?<div>
+          <Cabecera/>
+          <Buttons onClick={iniciar}>Començar</Buttons>
+        </div>
+        :<div>
+          <div>
             <Buttons onClick={back}>Anterior</Buttons>
             <Buttons onClick={next}>Següent</Buttons>
-        </div>
-        <div>
-          {textos.map((texto, indice) => {
-            const esActiu = indice === currentActive;
-            return <Escena key = {indice}  actiu = {esActiu} textoEscena = {texto}/>
-          })}
-        </div>
+          </div>
+          <div>
+            {textos.map((texto, indice) => {
+              const esActiu = indice === currentActive;
+              return <Escena key={indice} actiu={esActiu} textoEscena={texto} />;
+            })}
+          </div>
+      </div>
+      }
+   
     </div>
   );
 }
---
+++ src/components/cabecera.jsx
@@ -0,0 +1,7 @@
+export default function Cabecera() {
+    return <>
+        <h1>Benvingut</h1>
+        <p>Comença l'exercici. 
+        </p>
+    </>
+}
